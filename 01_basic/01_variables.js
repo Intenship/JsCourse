698317613e407517b781ea05a7c3511accc5a362
Intenship <projectintenship@gmail.com>
2025-02-19 07:38:17 +0000

    1 2 changes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 123323;
+let accountEmail = "sjskfj@gmail.com";
+var accountPassword ="1234";
+accoutCity ="Chiplun";
+let accountState="";
+
+// accountId=2
+accountEmail='shreys@gmail.com';
+accountPassword="1434";
+accoutCity="khed"
+
+
+
+console.table([accountId,accountEmail,accountPassword,accoutCity,accountState]);
+console.log(accountId)
